3ad0f159fb1f3330c7c60553e8ad5e7474498303
Erica Salling <esalling23@gmail.com>
2019-08-10 12:14:04 -0400

    V1: Template setup, message models/routes
    - Setup express api template
    - Message model with text field and user ownership
    - Message routes for CRUD
    - Populate owner on create for sending data to frontend
    - Front end receives successfully created message and sends socket event back
    - Backend listens for message creation socket message and broadcasts event
    to all clients

---
+++ server.js
@@ -0,0 +1,102 @@
+// require necessary NPM packages
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+// require route files
+const exampleRoutes = require('./app/routes/example_routes')
+const userRoutes = require('./app/routes/user_routes')
+const msgRoutes = require('./app/routes/message_routes')
+
+// require message api file
+const { index } = require('./lib/message_api')
+
+// require error handling middleware
+const errorHandler = require('./lib/error_handler')
+
+// require database configuration logic
+// `db` will be the actual Mongo URI as a string
+const db = require('./config/db')
+
+// require configured passport authentication middleware
+const auth = require('./lib/auth')
+
+// establish database connection
+mongoose.Promise = global.Promise
+mongoose.connect(db, {
+  useMongoClient: true
+})
+
+// instantiate express application object
+const app = express()
+
+// set CORS headers on response from this API using the `cors` NPM package
+// `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:7165' }))
+
+// define port for API to run on
+const port = process.env.PORT || 4741
+
+// this middleware makes it so the client can use the Rails convention
+// of `Authorization: Token token=<token>` OR the Express convention of
+// `Authorization: Bearer <token>`
+app.use((req, res, next) => {
+  if (req.headers.authorization) {
+    const auth = req.headers.authorization
+    // if we find the Rails pattern in the header, replace it with the Express
+    // one before `passport` gets a look at the headers
+    req.headers.authorization = auth.replace('Token token=', 'Bearer ')
+  }
+  next()
+})
+
+// register passport authentication middleware
+app.use(auth)
+
+// add `bodyParser` middleware which will parse JSON requests into
+// JS objects before they reach the route files.
+// The method `.use` sets up middleware for the Express application
+app.use(bodyParser.json())
+// this parses requests sent by `$.ajax`, which use a different content type
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// register route files
+app.use(exampleRoutes)
+app.use(userRoutes)
+app.use(msgRoutes)
+
+// register error handling middleware
+// note that this comes after the route middlewares, because it needs to be
+// passed any error messages from them
+app.use(errorHandler)
+
+// run API on designated port (4741 in this case)
+const http = require('http').Server(app)
+
+// require the socket.io module
+const io = require('socket.io')(http)
+
+// const socket = io
+// create an event listener
+console.log(io)
+// To listen to messages
+io.on('connection', (socket) => {
+  // console.log('user connected', socket.id)
+  // Created message
+  socket.on('newMessage', (msg) => {
+    console.log('user messaged!', msg)
+    io.emit('newMessage', {
+      message: msg.message,
+      from: socket.id
+    })
+  })
+})
+
+// wire up the server to listen to our port
+http.listen(port, () => {
+  console.log('connected to port: ' + port)
+})
+
+// needed for testing
+module.exports = app
